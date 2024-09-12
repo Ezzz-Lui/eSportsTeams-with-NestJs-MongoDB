7122189750087de43460dd130079c9c7527e0e19
Ezzz-Lui <ogn.lui@gmail.com>
2024-09-12 16:30:06 -0600

    Refactor: Add validation to CreateTeamDto

--- src/teams/dto/create-team.dto.ts
+++ src/teams/dto/create-team.dto.ts
@@ -1,1 +1,14 @@
-export class CreateTeamDto {}
+import { IsInt, IsNumber, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+export class CreateTeamDto {
+
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    idTeam: number;
+
+    @IsString()
+    @MinLength(1)
+    name:string;
+
+}
