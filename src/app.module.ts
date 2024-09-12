aa506a96d42e33444710a13c17142ec01c6a6fc8
Ezzz-Lui <ogn.lui@gmail.com>
2024-09-12 01:47:46 -0600

    Refactor: Add Mongoose module for MongoDB setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { join } from 'path'; //Native from node js
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { TeamsModule } from './teams/teams.module';
+import { Mongoose } from 'mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 @Module({
@@ -10,6 +12,10 @@ import { TeamsModule } from './teams/teams.module';
       rootPath: join(__dirname,'..','public'),
       exclude: ['/(.*)'],
     }),
+
+    MongooseModule.forRoot('mongodb://localhost:27017/esports-db'),
+
+
     TeamsModule
   ],
   controllers: [],
