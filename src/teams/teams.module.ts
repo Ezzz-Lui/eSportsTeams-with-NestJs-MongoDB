54b1285bb82bc8e10f397b3d70710e2b6ee22d16
Ezzz-Lui <ogn.lui@gmail.com>
2024-09-12 02:13:28 -0600

    Refactor: Add Mongoose schema

--- src/teams/teams.module.ts
+++ src/teams/teams.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TeamsService } from './teams.service';
 import { TeamsController } from './teams.controller';
+import { MongooseModule, Schema } from '@nestjs/mongoose';
+import { Team, TeamsSchema } from './entities/team.entity';
 
 @Module({
   controllers: [TeamsController],
   providers: [TeamsService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Team.name,
+        schema: TeamsSchema,
+      }
+    ]
+    )
+  ]
 })
 export class TeamsModule {}
