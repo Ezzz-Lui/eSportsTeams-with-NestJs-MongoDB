e5cdba87eb7a99314d7246df75beb9621dee3c8c
Ezzz-Lui <ogn.lui@gmail.com>
2024-09-12 16:30:21 -0600

    Refactor: Add Mongoose schema and lowercase team name in create method

--- src/teams/teams.service.ts
+++ src/teams/teams.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTeamDto } from './dto/create-team.dto';
 import { UpdateTeamDto } from './dto/update-team.dto';
+import { Team } from './entities/team.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class TeamsService {
-  create(createTeamDto: CreateTeamDto) {
-    return 'This action adds a new team';
+
+  constructor(
+    @InjectModel( Team.name )
+    readonly teamModel: Model<Team>
+  ){
+
+  }
+
+  async create(createTeamDto: CreateTeamDto) {
+    createTeamDto.name = createTeamDto.name.toLowerCase();
+
+    const team = await this.teamModel.create( createTeamDto );
+
+    return team;
   }
 
   findAll() {
