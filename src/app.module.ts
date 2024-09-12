93b5c75a7f1072cd68cceb81b277b200b62a498b
Ezzz-Lui <ogn.lui@gmail.com>
2024-09-12 00:39:09 -0600

    Build: New resource for teams

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { join } from 'path'; //Native from node js
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { TeamsModule } from './teams/teams.module';
 
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public')
-    })
+    }),
+    TeamsModule
   ],
   controllers: [],
   providers: [],
