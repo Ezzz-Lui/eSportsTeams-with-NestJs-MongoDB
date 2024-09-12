26a0cd8540989f122df49c6ed159a75808cbd7e6
Ezzz-Lui <ogn.lui@gmail.com>
2024-09-12 02:13:07 -0600

    Refactor: Add Mongoose schema for Team entity

--- src/teams/entities/team.entity.ts
+++ src/teams/entities/team.entity.ts
@@ -1,1 +1,21 @@
-export class Team {}
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+@Schema()
+export class Team extends Document {
+    // mongoId: string => mongo ya lo da
+    @Prop({
+        unique: true,
+        index: true
+    })
+    name: string;
+    
+    @Prop({
+        unique: true,
+        index: true
+    })
+    idTeam: string;
+}
+
+
+export const TeamsSchema = SchemaFactory.createForClass( Team );
